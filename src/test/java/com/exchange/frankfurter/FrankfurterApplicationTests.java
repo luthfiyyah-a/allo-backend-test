98fe8fbde552bc5c98b3cb97c3632a45cf7505f4
luthfiyyah-a <luthfiyyah.hanifah@gmail.com>
2026-02-17 00:09:15 +0700

    init project

---
+++ src/test/java/com/exchange/frankfurter/FrankfurterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exchange.frankfurter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrankfurterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
